1a9283d76e6cf796abd98baa1543bf8e6df1e5b2
Oindrilla-Mrunali <oindrillamrunali@gmail.com>
2024-06-28 14:09:36 +0530

    this is a new file

---
+++ variable_declaration.py
@@ -0,0 +1,14 @@
+num1 = 20
+name = "Oindrilla Mrunali"
+status = True
+print(type(num1))
+print(type(name))
+print(type(status))
+num2=10
+sum=num1+num2
+print(sum)
+print(name)
+#print(num1+name)
+"""
+hi siiirrrrr kyunnnnnn itna nhi jhel pa rhe hain hum
+"""
